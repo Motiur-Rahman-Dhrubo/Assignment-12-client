0eedecbbba757dd6b591852973266a1d75891b86
motiur-rahman-dhrubo <mdmotiurrahman383@gmail.com>
2025-05-01 19:13:23 +0600

    change some css in announcement page

--- src/components/Announcements/Announcements.jsx
+++ src/components/Announcements/Announcements.jsx
@@ -5,10 +5,10 @@ const Announcements = () => {
     const [allAnnouncements] = useAnnouncement();
 
     return (
-        <div className="w-11/12 mx-auto mt-2 md:mt-5 flex flex-col md:gap-4 gap-3">
+        <div className="w-11/12 mx-auto mt-2 md:mt-5 lg:mt-10 flex flex-col md:gap-4 gap-3">
             {
                 allAnnouncements.map((announcement) => (
-                    <div key={announcement._id} className="border p-4 rounded-lg">
+                    <div key={announcement._id} className="border p-4">
                         <h2 className="md:text-xl text-base font-semibold">{announcement?.announcementTitle}</h2>
                         <p className="md:text-base text-sm mt-2">{announcement?.announcementDescription}</p>
                     </div>
