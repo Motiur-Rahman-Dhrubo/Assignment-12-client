025d0e55863f136643e4374c7c51203a114bc68b
motiur-rahman-dhrubo <mdmotiurrahman383@gmail.com>
2025-05-01 18:06:04 +0600

    change border round of cupon section

--- src/components/AvailableCoupons/AvailableCoupons.jsx
+++ src/components/AvailableCoupons/AvailableCoupons.jsx
@@ -8,7 +8,7 @@ const AvailableCoupons = () => {
     const [copiedCode, setCopiedCode] = useState("");
 
     return (
-        <div className='mt-8 md:mt-12 w-11/12 mx-auto border p-4 rounded-lg bg-base-200'>
+        <div className='mt-8 md:mt-12 w-11/12 mx-auto border p-4 bg-base-200'>
             <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold text-center text-black">
                 Discount Coupons
             </h2>
@@ -16,7 +16,7 @@ const AvailableCoupons = () => {
             <div className="md:mt-5 mt-3 flex gap-4 justify-center flex-wrap">
                 {
                     availableCoupons?.map((coupon) => (
-                        <div key={coupon._id} className="border w-[200px] p-4 rounded-lg flex flex-col bg-white">
+                        <div key={coupon._id} className="border w-[200px] p-4 flex flex-col bg-white">
                             <h2 className="text-6xl text-center font-semibold">{coupon.discountPercentage}%</h2>
                             <p className="mt-2"><span className="font-semibold">Code:</span> {coupon.couponCode}</p>
                             <p className="font-semibold">Description:</p>
